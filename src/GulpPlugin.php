e0d3e742fa2bdf9599f72f86d7e268c8a6677511
Bernhard Schussek <bschussek@gmail.com>
2015-09-23 16:39:36 +0200

    Added initial GulpPlugin class

---
+++ src/GulpPlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the %%Project%% package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\GulpPlugin;
+
+use Puli\Manager\Api\Event\PuliEvents;
+use Puli\Manager\Api\Puli;
+use Puli\Manager\Api\PuliPlugin;
+
+/**
+ * Integrates Puli with the Gulp frontend workflow tool.
+ *
+ * @since  1.0
+ *
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class GulpPlugin implements PuliPlugin
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(Puli $puli)
+    {
+        $dispatcher = $puli->getEventDispatcher();
+
+        // Add event listeners that generate the JSON file here
+        $dispatcher->addListener(PuliEvents::XXX, array($this, 'handleXxx'));
+    }
+}
